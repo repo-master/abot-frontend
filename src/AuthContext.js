ab365d45faf5c88c65429ecd51c5fdeb45ed2303
Narayan Bandodker <repomaster@phaidelta.com>
2023-03-31 15:52:30 +0530

    Auth context is_authenticated field

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -19,19 +19,23 @@ export const AuthContextProvider = ({ children }) => {
   const [ sessionData, setSessionData, updateSessionData ] = useContext(ClientSessionContext);
   const { decodedToken, isExpired } = useJwt(sessionData.auth_token);
 
+  //TODO: Delegate the following to a different method or class
   const [ user, setUser ] = useState({});
 
   useEffect(() => {
     if (decodedToken && !isExpired)
       setUser({
-        user_name: decodedToken.user_name
+        user_name: decodedToken.user_name,
+        is_authenticated: true
       });
     else
       setUser({
-        user_name: null
+        user_name: null,
+        is_authenticated: false
       });
   }, [decodedToken, isExpired]);
 
+  //TODO: Validate the token that is received
   const login = async (payload) => {
     const apiResponse = await authenticate(payload);
     updateSessionData({
